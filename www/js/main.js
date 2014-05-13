482d3d87beec80155c2bb686aad382b853d2b374
ryry6207 <simr1_12@uni.worc.ac.uk>
2014-05-13 12:37:43 +0100

    Deleted Items

--- www/js/main.js
+++ www/js/main.js
@@ -1,4 +1,4 @@
-// JavaScript Document2
+// Setup Submit
 
  function onSubmit(){
 	
@@ -31,6 +31,67 @@
 			
 			var sqlStr = 'INSERT INTO pillbox (male, female, none, name, surname, email, dob) VALUES (?, ?, ?, ?, ?, ?, ?)';
 			tx.executeSql(sqlStr, [male, female, none, name, surname, email, dob]);
+		});
+ }
+ 
+ function onTxError(tx, err){
+	var msgText;
+	if(err){
+	//Tell the user what happened
+		msgText = "TX: " + err.message + " (" + err .code +")";
+	}
+	else{
+		msgText = "TX: Unknown error";
+	}
+	console.error(msgText);
+	alert(msgText);
+}
+		
+function onTxSucess(tx, msg){
+			console.log("TX: success");
+		}
+		
+function onSqlSuccess(tx, res) {
+	alert("success");
+}
+
+function onSqlError(tx, err) {
+		
+		console.log(err.message);
+		alert("Error: "  + err.message);
+}
+
+
+// Medication Submit
+
+ function onSubmit2(){
+	
+	alert ("onSubmit2 called");
+	
+	//open database
+	var theDB = openDatabase("pbdb4", "1.0", "pill box", 3 * 1024 * 1024);
+	
+	//creating table 
+	theDB.transaction(function (tx) {
+		
+		
+		var sqlStr = 'CREATE TABLE IF NOT EXISTS pillbox (name TEXT, dosage TEXT, time TEXT, repeat TEXT)';
+			console.log(sqlStr);
+			tx.executeSql(sqlStr, [], onSqlSuccess, onSqlError);
+	 
+		 
+			alert("writng data");
+	
+			var id= 1;
+			var name=$('#name').val();
+			var dosage=$('#dosage').val();
+			var time=$('#time').val(); 
+			var repeat=$('#repeat').val();
+			
+			console.log(name);
+			
+			var sqlStr = 'INSERT INTO pillbox (name, dosage, time, repeat) VALUES (?, ?, ?, ?)';
+			tx.executeSql(sqlStr, [name, dosage, time, repeat]);
 		});
  }
  
